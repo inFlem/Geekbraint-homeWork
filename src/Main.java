2217a775e99c39b15785cd1b7a2cf32e5b115eb3
Eugene <savkin.eugene@gmail.com>
2021-03-29 19:14:46 +0500

    Обновил класс, и создал 2 дз

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import static lesson1.lesson1.*;
+import static lesson2.lesson2.*;
+
+
+public class Main {
+
+        public static void main(String[] args) {
+        //lesson1
+            calculate(1.0f, 2.2f, 3.3f ,4.4f);
+            task10and20(11, 12, 13, 20);
+            isPositiveOrNegative(1);
+            isNegative(1);
+            greetings("Eugene");
+            years(2020);
+        //lesson2
+            invertArray();
+            System.out.println("\n");
+            fillArray();
+            System.out.println("\n");
+            changeArray();
+            System.out.println("\n");
+            fillDiagonal();
+        }
+
+}
---
+++ src/lesson2/lesson2.java
@@ -0,0 +1,53 @@
+package lesson2;
+
+public class lesson2 {
+
+    public static void invertArray() {
+        int[] arr = {1, 1, 1, 0, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void fillArray() {
+        int[] arr = new int[8];
+        for (int i = 0, j = 0; i < arr.length; i++, j += 3) {
+            arr[i] = j;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void changeArray() {
+        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if ((i == j) || (i == 5 - 1 - j)) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
